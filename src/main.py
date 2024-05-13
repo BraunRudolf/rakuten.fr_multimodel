b81d79e2f562a0fee05a7344f6f4592920acbe3e
Rudolf Braun <braun.ru@outlook.de>
2024-05-13 18:38:53 +0200

    belongs to commit 05e53bc

--- src/main.py
+++ src/main.py
@@ -1,14 +1,16 @@
 import torch
 from torch import nn
-from torch.utils.data import Dataset, DataLoader
-# from torchtext.data.utils import get_tokenizer
+from torch.utils.data import DataLoader
 from torchtext.vocab import build_vocab_from_iterator
 from torch.nn.utils.rnn import pad_sequence 
 from sklearn.metrics import precision_score, recall_score, f1_score
 import pandas as pd
 from features.build_features import DataImporter, TextPreprocessor, ImagePreprocessor
 import spacy
-from collections import Counter
+import numpy as np
+from model.text_classifier import TextClassifier
+from dataset.dataset import RakutenTextDataset
+from dataset.preprocess import build_vocab, collate_fn
 
 data_importer = DataImporter()
 df = data_importer.load_data()
@@ -35,109 +37,18 @@ val_dataset.to_csv('./data/preprocessed/val_dataset.csv')
 train_file = "./data/preprocessed/train_dataset.csv"
 test_file = "./data/preprocessed/test_dataset.csv"
 val_file = "./data/preprocessed/val_dataset.csv"
-class TextIterator:
-    def __init__(self, file_path, text_column, chunk_size=100):
-        self.file_path = file_path
-        self.text_column = text_column
-        self.chunk_size = chunk_size
-    
-    def __iter__(self):
-        for chunk in pd.read_csv(self.file_path, usecols=[self.text_column], chunksize=self.chunk_size):
-            for text in chunk[self.text_column]:
-                yield text
-# Tokenize the text data
-# tokenizer = get_tokenizer('frensh')
-# tokenizer = spacy.load("fr_core_news_sm") #TODO: change to nlp to follow standard 
 
-def build_vocab(data, spacy_model="fr_core_news_sm"):
-    counter = Counter()
-    nlp = spacy.load(spacy_model)
-    for text in data:
-        doc = nlp(text)
-        tokens = [token.text for token in doc]
-        counter.update(tokens)
-    vocab = {token: index for index, (token, _) in enumerate(counter.most_common())}
-    return vocab, nlp
 vocab, nlp = build_vocab(train_dataset['description'].to_list())
 
-# text_iterator = TextIterator(train_file, 'description')
-# for chunk in text_iterator:
-#     doc = tokenizer(chunk)
-#     for token in doc:
-#         # Check if token has a vector before setting it
-#         if token.text in vocab:
-#             index = vocab[token.text].orth
-            # print("Token:", token.text, "Index:", index)
-# Build the vocabulary
-# def build_vocab(file_path):
-#     with open(file_path, 'r', encoding='utf-8') as f:
-#         tokens = tokenizer(f.read())
-#     return tokens
-
-# train_tokens = build_vocab(train_file)
-# vocab = build_vocab_from_iterator(train_tokens)
-
-
-class RakutenTextDataset(Dataset):
-    def __init__(self, file_path, text_column, label_column, vocab, spacy_model):
-        self.vocab = vocab
-        self.text_data, self.labels = self.load_data(file_path, text_column, label_column)
-        self.tokenizer =  spacy_model.tokenizer
-
-    def load_data(self, file_path, text_column, label_column):
-        # Load the CSV file
-        df = pd.read_csv(file_path)
-        text_data = df[text_column].tolist()
-        labels = df[label_column].tolist()
-        return text_data, labels
-    
-    def text_to_tensor(self, text):
-        tokens = self.tokenizer(text)
-        tensor = [self.vocab[token.text] for token in tokens if token.text in self.vocab]
-        return torch.tensor(tensor, dtype=torch.long) 
-
-    def __len__(self):
-        return len(self.text_data)
-    
-    def __getitem__(self, index):
-        text = self.text_data[index]
-        label = self.labels[index]
-        text_tensor = self.text_to_tensor(text)
-        return text_tensor, label
-
-class TextClassifier(nn.Module):
-    def __init__(self, vocab_size, embedding_dim, hidden_dim, num_classes):
-        super(TextClassifier, self).__init__()
-        self.embedding = nn.Embedding(vocab_size, embedding_dim)
-        self.rnn = nn.LSTM(embedding_dim, hidden_dim, batch_first=True)
-        self.fc = nn.Linear(hidden_dim, num_classes)
-        
-    def forward(self, x):
-        embedded = self.embedding(x)
-        output, _ = self.rnn(embedded)
-        last_hidden = output[:, -1, :]
-        logits = self.fc(last_hidden)
-        return logits
-def collate_fn(batch):
-    # Separate inputs and targets
-    inputs, targets = zip(*batch)
-
-    # Pad sequences to the maximum length in the batch
-    padded_inputs = pad_sequence(inputs, batch_first=True, padding_value=0)
-
-    # Convert targets to tensor
-    targets_tensor = torch.tensor(targets, dtype=torch.long)
-
-    return padded_inputs, targets_tensor
 
 #TRAINING
 # Training parameters
 vocab_size = len(vocab)
-embedding_dim = 100
+embedding_dim = 250
 hidden_dim = 128
 num_classes = 27
-batch_size = 32
-num_epochs = 10
+batch_size = 50
+num_epochs = 50
 learning_rate = 0.001
 
 # Create the model
@@ -148,25 +59,32 @@ criterion = nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
 # Create data loaders for the training and validation sets
-train_dataset = RakutenTextDataset(train_file, 'description', 'prdtypecode', vocab, nlp)#, tokenizer)
-valid_dataset = RakutenTextDataset(val_file, 'description', 'prdtypecode', vocab, nlp)# tokenizer.vocab, tokenizer)
+train_dataset = RakutenTextDataset(train_file, 'description', 'prdtypecode', vocab, nlp)
+valid_dataset = RakutenTextDataset(val_file, 'description', 'prdtypecode', vocab, nlp)
 
 train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, collate_fn=collate_fn)
 valid_loader = DataLoader(valid_dataset, batch_size=batch_size, shuffle=False, collate_fn=collate_fn)
 
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+model.to(device)
 # Iterate over the training data for the specified number of epochs
 for epoch in range(num_epochs):
+    print("Start Training")
     model.train()
     total_loss = 0.0
     total_samples = 0
+    correct = 0
     for inputs, targets in train_loader:
         optimizer.zero_grad()
-        inputs = inputs
-        targets = targets
+        inputs = inputs.to(device)
+        targets = targets.to(device)
         outputs = model(inputs)
         loss = criterion(outputs.view(-1, num_classes), targets.view(-1))
         loss.backward()
         optimizer.step()
+        #test 
+        predictions = torch.argmax(outputs, dim=1)
+        correct += (predictions == targets.view(-1)).sum().item()
 
         total_loss += loss.item() * len(inputs)
         total_samples += len(inputs)
@@ -175,20 +93,27 @@ for epoch in range(num_epochs):
     model.eval()
     total_val_loss = 0.0
     total_val_samples = 0
+    val_correct = 0
     with torch.no_grad():
         for inputs, targets in valid_loader:
-            inputs = inputs
-            targets = targets
+            inputs = inputs.to(device)
+            targets = targets.to(device)
             outputs = model(inputs)
             val_loss = criterion(outputs.view(-1, num_classes), targets.view(-1))
-
             total_val_loss += val_loss.item() * len(inputs)
             total_val_samples += len(inputs)
 
+            predictions = torch.argmax(outputs, dim=1)
+            val_correct += (predictions == targets.view(-1)).sum().item()
+
     avg_loss = total_loss / total_samples
+    # test
+    avg_acc = 100* correct / total_samples
+    avg_val_acc = 100* val_correct / total_samples
+
     avg_val_loss = total_val_loss / total_val_samples
 
-    print(f"Epoch {epoch+1}/{num_epochs}, Train Loss: {avg_loss:.4f}, Val Loss: {avg_val_loss:.4f}")
+    print(f"Epoch {epoch+1}/{num_epochs}, Train Loss: {avg_loss:.4f}, Train Acc: {avg_acc:.4f}, Val Loss: {avg_val_loss:.4f}, Val Acc: {avg_val_acc:.4f}")
 
 ### EVALUATION
 # Load the test set or a separate evaluation set
@@ -205,8 +130,8 @@ with torch.no_grad():
     targets_list = []
 
     for inputs, targets in test_loader:
-        iinputsnputs = inputs
-        targets = targets
+        inputs = inputs.to(device)
+        targets = targets.to(device)
         outputs = model(inputs)
 
         _, predicted_labels = torch.max(outputs, dim=1)
