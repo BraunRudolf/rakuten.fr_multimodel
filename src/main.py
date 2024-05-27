3886ef92c276ea11b9637bd57b952c5fbe7bcc8f
Rudolf Braun <braun.ru@outlook.de>
2024-05-27 14:19:47 +0200

    adjustment for adjusted dataimporter commit 097e3f0

--- src/main.py
+++ src/main.py
@@ -1,4 +1,7 @@
+#!/usr/bin/env python
 import os
+from numpy import save
+from sqlalchemy import label
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -10,26 +13,28 @@ from model.train_model import train_text_model, validate_text_model, evaluate_te
 from dataset.dataset import RakutenTextDataset
 from dataset.preprocess import build_vocab, load_vocab_and_nlp, collate_fn
 from callbacks.callbacks import LearningRateScheduler, EarlyStopping
+from dataset.preprocess import train_val_test_indices
 import mlflow
 import dotenv
 
 dotenv.load_dotenv()
 
 #TODO: Create function that creates splits based on pct
-data_importer = DataImporter()
+data_importer = DataImporter(target_col=['designation', 'description'], label_col='label')
 df = data_importer.load_data()
 X_train, X_val, X_test, y_train, y_val, y_test = data_importer.split_train_test(df)
-
 #TODO: create separate preprocessing functions and make usable by dataloader
 # Preprocess text and images
 text_preprocessor = TextPreprocessor()
 image_preprocessor = ImagePreprocessor()
-text_preprocessor.preprocess_text_in_df(X_train, columns=["description"])
-text_preprocessor.preprocess_text_in_df(X_val, columns=["description"])
-text_preprocessor.preprocess_text_in_df(X_test, columns=["description"])
-image_preprocessor.preprocess_images_in_df(X_train)
-image_preprocessor.preprocess_images_in_df(X_val)
-image_preprocessor.preprocess_images_in_df(X_test)
+
+
+X_train = text_preprocessor.preprocess_text_in_df(X_train)
+X_val = text_preprocessor.preprocess_text_in_df(X_val)
+X_test = text_preprocessor.preprocess_text_in_df(X_test)
+
+#HACK: test as '' strings which load as NaN. check why and resolve
+X_test = X_test.replace('',' ')
 
 #TODO: remove in future
 # Write Preprocessed Files
@@ -40,17 +45,22 @@ train_dataset.to_csv('./data/preprocessed/train_dataset.csv')
 test_dataset.to_csv('./data/preprocessed/test_dataset.csv')
 val_dataset.to_csv('./data/preprocessed/val_dataset.csv')
 
+test = pd.read_csv("./data/preprocessed/test_dataset.csv")
+
 #TODO: only one file
 train_file = "./data/preprocessed/train_dataset.csv"
 test_file = "./data/preprocessed/test_dataset.csv"
 val_file = "./data/preprocessed/val_dataset.csv"
 
-save_path = os.getenv("SAVE_PATH")
-if len(os.listdir(save_path)) == 0:
+assert val_dataset['target'].isna().sum() == 0, 'none value'
+
+vocab_path = os.getenv("VOCAB_PATH")
+#HACK change to more precise check
+if len(os.listdir(vocab_path)) == 0:
     # create new tokenizer + vocab
-    vocab, nlp = build_vocab(train_dataset['description'].to_list())
+    vocab, nlp = build_vocab(train_dataset['target'].to_list(), save_dir=vocab_path)
 else:
-    vocab, nlp = load_vocab_and_nlp(save_path)
+    vocab, nlp = load_vocab_and_nlp(vocab_path)
 
 #TRAINING
 # Training parameters
@@ -58,7 +68,7 @@ vocab_size = len(vocab)
 embedding_dim = 250
 hidden_dim = 128
 num_classes = 27
-batch_size = 50
+batch_size = 32
 num_epochs = 200
 learning_rate = 0.001
 
@@ -70,12 +80,12 @@ mlflow.set_experiment(MLFLOW_EXPERIMENT_NAME)
 
 
 # Create data loaders for the training and validation sets
-train_dataset = RakutenTextDataset(train_file, 'description', 'prdtypecode', vocab, nlp)
-valid_dataset = RakutenTextDataset(val_file, 'description', 'prdtypecode', vocab, nlp)
+train_dataset = RakutenTextDataset(train_file, 'target', 'label', vocab, nlp)
+valid_dataset = RakutenTextDataset(val_file, 'target', 'label', vocab, nlp)
+test_dataset = RakutenTextDataset(test_file, 'target', 'label', vocab, nlp)
 
 train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, collate_fn=collate_fn)
 val_loader = DataLoader(valid_dataset, batch_size=batch_size, shuffle=False, collate_fn=collate_fn)
-test_dataset = RakutenTextDataset(test_file, 'description', 'prdtypecode', vocab, nlp)# tokenizer.vocab, tokenizer)
 test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True, collate_fn=collate_fn)
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -88,7 +98,7 @@ optimizer = optim.Adam(model.parameters(), lr=0.001)
 criterion = nn.CrossEntropyLoss()
 # Define Callbacks
 scheduler = LearningRateScheduler(optimizer)
-early_stopping = EarlyStopping()
+early_stopping = EarlyStopping(verbose=True)
 
 params = {
     "vocab_size": len(vocab),
@@ -109,8 +119,8 @@ with mlflow.start_run():
         train_loss, train_accuracy = train_text_model(model, train_loader, optimizer, criterion, device, params['num_classes'])
         val_loss, val_accuracy = validate_text_model(model, val_loader, criterion, device, params['num_classes'])
         
-        scheduler.on_epoch_end(epoch, val_loss)
-        early_stopping(val_loss)
+        lr_adjusted = scheduler.on_epoch_end(epoch, val_loss)
+        early_stopping(val_loss, lr_adjusted=lr_adjusted)
 
         print(f'Epoch {epoch + 1}/{num_epochs}, '
               f'Train Loss: {train_loss:.4f}, Train Accuracy: {train_accuracy:.2f}%, '
