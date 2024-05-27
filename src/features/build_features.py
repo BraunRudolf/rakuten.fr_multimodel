097e3f03c6056eb25b47e025a182004e3a76e7ee
Rudolf Braun <braun.ru@outlook.de>
2024-05-27 14:18:49 +0200

    Adjust DataImporter to new project data setup

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -1,40 +1,52 @@
+import os
 import pandas as pd
+from typing import Union, List
 import nltk
 from bs4 import BeautifulSoup
 import re
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
-import pickle
 import math
 
 
 class DataImporter:
-    def __init__(self, filepath="data/preprocessed"):
-        self.filepath = filepath
+    #TODO:change target_col to feature_col
+    def __init__(self, target_col: Union[str, List[str]], label_col: str,
+                 datapath: str = "data/preprocessed", filename: str = 'dataset.csv'):
+        self.filepath = os.path.join(datapath, filename)
+        self.datapath = datapath
+        self.filename = filename
+        self.target_col = target_col
+        self.label_col = label_col
 
     def load_data(self):
-        data = pd.read_csv(f"{self.filepath}/X_train_update.csv")
-        data["description"] = data["designation"] + str(data["description"])
-        data = data.drop(["Unnamed: 0", "designation"], axis=1)
+        try:
+            data = pd.read_csv(self.filepath, index_col=0)
+        except FileNotFoundError:
+            raise FileNotFoundError(f"File data['target'] = data['target'].fillna(''not found at {self.filepath}")
+        except pd.errors.EmptyDataError:
+            raise ValueError(f"No data found in {self.filepath}")
+        except Exception as e:
+            raise e
 
-        target = pd.read_csv(f"{self.filepath}/Y_train_CVw08PX.csv")
-        target = target.drop(["Unnamed: 0"], axis=1)
-        modalite_mapping = {
-            modalite: i for i, modalite in enumerate(target["prdtypecode"].unique())
-        }
-        target["prdtypecode"] = target["prdtypecode"].replace(modalite_mapping)
+        for col in self.target_col:
+            data[col] = data[col].fillna('')
 
-        with open("models/mapper.pkl", "wb") as fichier:
-            pickle.dump(modalite_mapping, fichier)
+        if isinstance(self.target_col, list):
+            data['target'] = data[self.target_col[0]].astype(str)
+            for col in self.target_col[1:]:
+                data['target'] += " " + data[col].astype(str)
+        else:
+            data['target'] = data[self.target_col].astype(str)
 
-        df = pd.concat([data, target], axis=1)
+        data['label'] = data[self.label_col]
 
-        return df
+        return data[['target', 'label']]
 
     def split_train_test(self, df, samples_per_class=600):
 
-        grouped_data = df.groupby("prdtypecode")
+        grouped_data = df.groupby("label")
 
         X_train_samples = []
         X_test_samples = []
@@ -52,23 +64,23 @@ class DataImporter:
         X_train = X_train.sample(frac=1, random_state=42).reset_index(drop=True)
         X_test = X_test.sample(frac=1, random_state=42).reset_index(drop=True)
 
-        y_train = X_train["prdtypecode"]
-        X_train = X_train.drop(["prdtypecode"], axis=1)
+        y_train = X_train["label"]
+        X_train = X_train["target"]
 
-        y_test = X_test["prdtypecode"]
-        X_test = X_test.drop(["prdtypecode"], axis=1)
+        y_test = X_test["label"]
+        X_test = X_test["target"]
 
         val_samples_per_class = 50
 
-        grouped_data_test = pd.concat([X_test, y_test], axis=1).groupby("prdtypecode")
+        grouped_data_test = pd.concat([X_test, y_test], axis=1).groupby("label")
 
         X_val_samples = []
         y_val_samples = []
 
         for _, group in grouped_data_test:
             samples = group.sample(n=val_samples_per_class, random_state=42)
-            X_val_samples.append(samples[["description", "productid", "imageid"]])
-            y_val_samples.append(samples["prdtypecode"])
+            X_val_samples.append(samples["target"])
+            y_val_samples.append(samples["label"])
 
         X_val = pd.concat(X_val_samples)
         y_val = pd.concat(y_val_samples)
@@ -76,6 +88,10 @@ class DataImporter:
         X_val = X_val.sample(frac=1, random_state=42).reset_index(drop=True)
         y_val = y_val.sample(frac=1, random_state=42).reset_index(drop=True)
 
+        assert X_train.isna().sum() == 0
+        assert X_val.isna().sum() == 0
+        assert X_test.isna().sum() == 0
+        
         return X_train, X_val, X_test, y_train, y_val, y_test
 
 
@@ -101,12 +117,12 @@ class TextPreprocessor:
         self.lemmatizer = WordNetLemmatizer()
         self.stop_words = set(
             stopwords.words("french")
-        )  # Vous pouvez choisir une autre langue si nécessaire
-
-    def preprocess_text(self, text):
+        )
+    def preprocess_text(self, text:str):
 
         if isinstance(text, float) and math.isnan(text):
             return ""
+        
         # Supprimer les balises HTML
         text = BeautifulSoup(text, "html.parser").get_text()
 
@@ -123,9 +139,14 @@ class TextPreprocessor:
             if word not in self.stop_words
         ]
 
-        return " ".join(filtered_words[:10])
+        return " ".join(filtered_words[:100])
 
-    def preprocess_text_in_df(self, df, columns):
-        for column in columns:
-            df[column] = df[column].apply(self.preprocess_text)
+    def preprocess_text_in_df(self, df:Union[pd.DataFrame,pd.Series], columns:List[str]=['']):
+        if isinstance(df, pd.DataFrame):
+            for column in columns:
+                df[column] = df[column].apply(self.preprocess_text)
+            return df
+
+        if isinstance(df, pd.Series):
+            return df.apply(self.preprocess_text)
 
